3abe0e35ad440df51bdcd9bf752cf3969ce9e93c
aythamirg <aythami.r.g@hotmail.com>
2023-03-15 19:17:44 +0000

    Primero

--- src/main/java/com/mycompany/mavenfreestyle/MavenFreeStyle.java
+++ src/main/java/com/mycompany/mavenfreestyle/MavenFreeStyle.java
@@ -14,20 +14,14 @@ public class MavenFreeStyle {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-    
-    public int fibonacci(int a){
 
-	if (a == 0) {
-
-        return 0;
-
-    } else if (a == 1) {
-
-        return 1;
-
-    } else{
-            return (fibonacci(a-1) + fibonacci(a-2));
-	}
-
+    public int fibonacci(int a) {
+        if (a == 0) {
+            return 0;
+        } else if (a == 1) {
+            return 1;
+        } else {
+            return (fibonacci(a - 1) + fibonacci(a - 2));
+        }
     }
 }
